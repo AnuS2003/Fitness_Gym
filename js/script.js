c29d1613ea6e45d4eee430b23455c2dd113c7104
AnuS2003 <21a04.anupama@sjec.ac.in>
2024-06-15 09:01:09 +0530

    Files uploaded

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+const body = document.querySelector('body');
+
+const mobileMenu = () => {
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+    body.classList.toggle('active');
+};
+
+menu.addEventListener('click', mobileMenu);
